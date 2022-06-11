c2a25219083e0a08db31b72d54b6604c17ffd39c
30james00 <48206982+30james00@users.noreply.github.com>
2022-06-11 12:20:29 +0200

    Add printing character codes

--- lib/application/huffman.dart
+++ lib/application/huffman.dart
@@ -1,35 +1,61 @@
 import 'package:collection/collection.dart';
 
-Map<String, int> count(String phrase) {
-  Map<String, int> result = <String, int>{};
-  for (int c in phrase.runes) {
-    result.update(String.fromCharCode(c), (value) => value + 1,
-        ifAbsent: () => 1);
-  }
-  return result;
-}
-
 class Huffman {
-  Huffman(Map<String, int> phrase) {
+  HuffmanNode? root;
+
+  Huffman(String phrase) {
+    Map<String, int> freq = _count(phrase);
+
     PriorityQueue<HuffmanNode> queue =
         PriorityQueue((a, b) => a.frequency.compareTo(b.frequency));
-    phrase.forEach((key, value) {
+    freq.forEach((key, value) {
       queue.add(HuffmanNode(key, value, null, null));
     });
+
+    while (queue.length > 1) {
+      HuffmanNode x = queue.removeFirst();
+      HuffmanNode y = queue.removeFirst();
+
+      HuffmanNode f = HuffmanNode('-', x.frequency + y.frequency, x, y);
+
+      root = f;
+
+      queue.add(f);
+    }
+
+    if (root != null) printCode(root!, "");
+  }
+
+  Map<String, int> _count(String phrase) {
+    Map<String, int> result = <String, int>{};
+    for (int c in phrase.runes) {
+      result.update(String.fromCharCode(c), (value) => value + 1,
+          ifAbsent: () => 1);
+    }
+    return result;
   }
 }
 
 class HuffmanNode {
-  final String letter;
+  final String character;
   final int frequency;
 
   HuffmanNode? left;
   HuffmanNode? right;
 
-  HuffmanNode(this.letter, this.frequency, this.left, this.right);
+  HuffmanNode(this.character, this.frequency, this.left, this.right);
 
   @override
   String toString() {
-    return '$letter $frequency';
+    return '$character $frequency';
+  }
+}
+
+void printCode(HuffmanNode node, String s) {
+  if (node.left == null && node.right == null)
+    print('${node.character} : $s');
+  else {
+    printCode(node.left!, '${s}0');
+    printCode(node.right!, '${s}1');
   }
 }
--- test/application/huffman_test.dart
+++ test/application/huffman_test.dart
@@ -2,13 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:huffman/application/huffman.dart';
 
 void main() {
-  test("Test counting", () {
-    var result = count("Sbinnn");
-    expect(result['S'], 1);
-    expect(result['n'], 3);
-  });
+
   test("Test Huffman construction", () {
-    var huffman = Huffman(count("W czasie suszy szosza sucha"));
-    expect(huffman, isNotNull);
+    var huffman = Huffman("BCAADDDCCACACAC");
+    expect(huffman.root?.frequency, 15);
   });
 }
