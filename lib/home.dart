b6dbd22c8d82db770a2130598f96da49cdd6537a
30james00 <48206982+30james00@users.noreply.github.com>
2022-06-13 14:58:08 +0200

    Add UI

--- lib/home.dart
+++ lib/home.dart
@@ -1,16 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:huffman/application/huffman.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _formKey = GlobalKey<FormState>();
+  final formController = TextEditingController();
+  Huffman? huffman;
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    formController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Huffman'),
-      ),
-      body: const Center(
-        child: Text('Huffman'),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Huffman code',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+            Form(
+              key: _formKey,
+              child: TextFormField(
+                controller: formController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter message to encode';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Processing Data'),
+                    ),
+                  );
+                  setState(() {
+                    huffman = Huffman(formController.text);
+                  });
+                }
+              },
+              child: Text(
+                'Encode',
+                style: Theme.of(context).textTheme.button,
+              ),
+            ),
+            if (huffman != null) Text(huffman!.encoded),
+          ],
+        ),
       ),
     );
   }
